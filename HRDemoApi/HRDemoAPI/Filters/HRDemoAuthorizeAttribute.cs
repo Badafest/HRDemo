6da771e15679bcfe9e435646d088f8b8251958ab
badafest <er.sandipdahal@gmail.com>
2025-03-05 14:04:29 +0545

    fix authorize filter in old project

--- HRDemoApi/HRDemoAPI/Filters/HRDemoAuthorizeAttribute.cs
+++ HRDemoApi/HRDemoAPI/Filters/HRDemoAuthorizeAttribute.cs
@@ -1,4 +1,5 @@
 ﻿using HRDemoAPI.Data;
+using HRDemoAPI.Utilities;
 using System.Linq;
 using System.Security.Claims;
 using System.Web.Http;
@@ -27,21 +28,30 @@ namespace HRDemoAPI.Filters
                 .AsNoTracking()
                 .FirstOrDefault(u => u.Name == identity.Name);
 
-            if (user != null)
+            if (user == null)
             {
-                var role = user.Role.ToString();
-                identity.AddClaim(new Claim("userRole", role));
-
-                identity.AddClaim(new Claim(identity.RoleClaimType, role));
-
-                var managedDepartments = _hRDemoApiDb.Departments.Where(d => d.ManagerID !=null && d.ManagerID == user.EmployeeID).Select(d => d.DepartmentID);
-                foreach (var departmentId in managedDepartments)
-                {
-                    identity.AddClaim(new Claim(identity.RoleClaimType, $"Manager-{departmentId}"));
-                }
-
-                identity.AddClaim(new Claim("displayName", $"{user.Employee.FirstName} {user.Employee.LastName}"));
+                actionContext.Response = HttpUtilities.CreateResponseMessage($"User {identity?.Name} not found in the database", System.Net.HttpStatusCode.Unauthorized);
+                return;
             }
+
+            if (user.Employee == null)
+            {
+                actionContext.Response = HttpUtilities.CreateResponseMessage($"User {identity?.Name} is not mapped to any employee", System.Net.HttpStatusCode.Unauthorized);
+                return;
+            }
+
+            var role = user.Role.ToString();
+            identity.AddClaim(new Claim("userRole", role));
+
+            identity.AddClaim(new Claim(identity.RoleClaimType, role));
+
+            var managedDepartments = _hRDemoApiDb.Departments.Where(d => d.ManagerID !=null && d.ManagerID == user.EmployeeID).Select(d => d.DepartmentID);
+            foreach (var departmentId in managedDepartments)
+            {
+                identity.AddClaim(new Claim(identity.RoleClaimType, $"Manager-{departmentId}"));
+            }
+
+            identity.AddClaim(new Claim("displayName", $"{user.Employee.FirstName} {user.Employee.LastName}"));
         }
     }
 
