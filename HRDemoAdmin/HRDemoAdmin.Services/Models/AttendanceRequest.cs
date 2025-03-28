8c91e8e5e99fd2b1d443519e8121c0183f33a9c5
badafest <er.sandipdahal@gmail.com>
2025-03-28 15:30:14 +0545

    add views for attendance

--- HRDemoAdmin/HRDemoAdmin.Services/Models/AttendanceRequest.cs
+++ HRDemoAdmin/HRDemoAdmin.Services/Models/AttendanceRequest.cs
@@ -1,10 +1,17 @@
-﻿namespace HRDemoAdmin.Services.Models
+﻿using DataAnnotationsExtensions;
+using System.ComponentModel;
+
+namespace HRDemoAdmin.Services.Models
 {
     public class AttendanceRequest
     {
         public System.DateTimeOffset date { get; set; }
+        [DisplayName("Check In")]
         public System.DateTimeOffset checkIn { get; set; }
+        [DisplayName("Check Out")]
         public System.DateTimeOffset checkOut { get; set; }
         public int employeeId { get; set; }
+        [Email, DisplayName("Employee Email")]
+        public string employeeEmail { get; set; }
     }
 }
--- HRDemoAdmin/HRDemoAdmin/Controllers/AttendancesController.cs
+++ HRDemoAdmin/HRDemoAdmin/Controllers/AttendancesController.cs
@@ -1,13 +1,122 @@
-﻿using System.Web.Mvc;
+﻿using HRDemoAdmin.Services;
+using HRDemoAdmin.Services.Models;
+using System;
+using System.Web.Mvc;
 
 namespace HRDemoAdmin.Controllers
 {
     public class AttendancesController : ControllerBase
     {
-        // GET: Attendances
-        public ActionResult Index()
+        private readonly AttendanceService _attendanceService;
+        public AttendancesController()
         {
+            var apiBaseUrl = System.Configuration.ConfigurationManager.AppSettings["HRDemoApiBaseUrl"];
+            _attendanceService = new AttendanceService(apiBaseUrl);
+        }
+        // GET: Attendances
+        public ActionResult Index(string employeeEmail, DateTimeOffset? startDate = null, DateTimeOffset? endDate = null)
+        {
+            int employeeId = 0;
+            if (!string.IsNullOrEmpty(employeeEmail))
+            {
+                var employee = _attendanceService.GetEmployeeByEmail(employeeEmail);
+                employeeId = employee?.EmployeeID ?? -1;
+            }
+
+            var minStartDate = DateTimeOffset.Now.Subtract(new TimeSpan(2 * 365, 0, 0, 0));
+            var maxEndDate = DateTimeOffset.Now;
+
+            startDate = (startDate != null && startDate > minStartDate) ? startDate : minStartDate;
+            endDate = (endDate != null && endDate < maxEndDate) ? endDate : maxEndDate;
+
+            var response = _attendanceService.GetAttendances(employeeId, startDate, endDate);
+            return HandleApiResponse(response, response.Data) ?? View(response.Data);
+        }
+
+        // GET: Attendances/{id}
+        public ActionResult Details(int id)
+        {
+            var response = _attendanceService.GetAttendanceDetails(id);
+            return HandleApiResponse(response, response.Data) ?? View(response.Data);
+        }
+
+        // Get: Attendances/Create
+        [HttpGet]
+        public ActionResult Create()
+        {
+
+            ViewData.Model = new AttendanceRequest();
             return View();
+        }
+
+        // POST: Attendances/Edit/{id}
+        [HttpPost, ValidateAntiForgeryToken]
+        public ActionResult Create(AttendanceRequest attendanceRequest)
+        {
+            if (string.IsNullOrEmpty(attendanceRequest.employeeEmail))
+            {
+                ModelState.AddModelError("employeeEmail", $"Employee email is required");
+                return View(attendanceRequest);
+            }
+
+            var employee = _attendanceService.GetEmployeeByEmail(attendanceRequest.employeeEmail);
+            if (employee == null)
+            {
+                ModelState.AddModelError("employeeEmail", $"Employee not found with email {attendanceRequest.employeeEmail}");
+                return View(attendanceRequest);
+            }
+
+            attendanceRequest.employeeId = employee.EmployeeID;
+
+            attendanceRequest.date = new DateTime(attendanceRequest.date.Year, attendanceRequest.date.Month, attendanceRequest.date.Day, 9, 0, 0);
+            attendanceRequest.checkIn = new DateTime(attendanceRequest.date.Year, attendanceRequest.date.Month, attendanceRequest.date.Day, attendanceRequest.checkIn.Hour, attendanceRequest.checkIn.Minute, attendanceRequest.checkIn.Second);
+            attendanceRequest.checkOut = new DateTime(attendanceRequest.date.Year, attendanceRequest.date.Month, attendanceRequest.date.Day, attendanceRequest.checkOut.Hour, attendanceRequest.checkOut.Minute, attendanceRequest.checkOut.Second);
+
+            var response = _attendanceService.CreateAttendance(attendanceRequest);
+            return HandleApiResponse(response, attendanceRequest) ?? RedirectToAction("Details", new { id = response.Data.AttendanceID });
+        }
+
+        // GET: Attendances/Edit/{id}
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            var response = _attendanceService.GetAttendanceDetails(id);
+            return HandleApiResponse(response, response.Data) ?? View(response.Data);
+        }
+
+        // POST: Attendances/Edit/{id}
+        [HttpPost, ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, AttendanceResponse attendanceResponse)
+        {
+            AttendanceRequest attendanceRequest = new AttendanceRequest
+            {
+                date = attendanceResponse.Date,
+                checkIn = attendanceResponse.CheckInTime,
+                checkOut = attendanceResponse.CheckOutTime
+            };
+
+            attendanceRequest.date = new DateTime(attendanceRequest.date.Year, attendanceRequest.date.Month, attendanceRequest.date.Day, 9, 0, 0);
+            attendanceRequest.checkIn = new DateTime(attendanceRequest.date.Year, attendanceRequest.date.Month, attendanceRequest.date.Day, attendanceRequest.checkIn.Hour, attendanceRequest.checkIn.Minute, attendanceRequest.checkIn.Second);
+            attendanceRequest.checkOut = new DateTime(attendanceRequest.date.Year, attendanceRequest.date.Month, attendanceRequest.date.Day, attendanceRequest.checkOut.Hour, attendanceRequest.checkOut.Minute, attendanceRequest.checkOut.Second);
+
+            var response = _attendanceService.EditAttendance(id, attendanceRequest);
+            return HandleApiResponse(response, attendanceResponse) ?? RedirectToAction("Details", new { id });
+        }
+
+        // GET: Attendances/Delete/{id}
+        [HttpGet]
+        public ActionResult Delete(int id)
+        {
+            var response = _attendanceService.GetAttendanceDetails(id);
+            return HandleApiResponse(response, response.Data) ?? View(response.Data);
+        }
+
+        // POST: Attendances/Delete/{id}
+        [HttpPost, ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, AttendanceResponse attendanceResponse)
+        {
+            var response = _attendanceService.DeleteAttendance(id);
+            return HandleApiResponse(response, response.Data) ?? RedirectToAction("Index");
         }
     }
 }
