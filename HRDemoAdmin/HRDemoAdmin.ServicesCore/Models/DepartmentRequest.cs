2e6973c2e8d9b35ab361c59750b9b61a02b795e3
badafest <er.sandipdahal@gmail.com>
2025-04-01 13:45:45 +0545

    keep legacy applications only

--- HRDemoAdmin/HRDemoAdmin.ServicesCore/Models/DepartmentRequest.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace HRDemoAdmin.Services.Models
-{
-    public class DepartmentRequest
-    {
-        public string name { get; set; } = "";
-        public string description { get; set; } = "";
-        public int managerId { get; set; }
-    }
-}
--- HRDemoReportService/HRDemoReportServiceCore/BindingInfo.cs
+++
@@ -1,8 +0,0 @@
-namespace HRDemoReportServiceCore
-{
-    public class BindingInfo
-    {
-        public string Endpoint { get; set; } = "/HRDemoReportService.svc";
-        public Binding Binding { get; set; } = new BasicHttpBinding(BasicHttpSecurityMode.None);
-    }
-}
